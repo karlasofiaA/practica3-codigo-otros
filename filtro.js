fdfc6b3b463c14f7bdf88efafebf19dbec6c4dec
Karla Arenas <karlasofia.arenasv@gmail.com>
2023-02-13 15:29:49 -0600

    ci: Correcciones al codigo de otros

--- filtro.js
+++ filtro.js
@@ -8,56 +8,64 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+const listaProductos = document.querySelector("#lista-de-productos"); /* Identificamos el selector como ID */
+const selector = document.querySelector('.input'); /* Cambiar el nombre de la variable */
 
+/* Este for nos muestra todos los productos con nombre, titulo e imagen */
 for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+  /* Cambiamos el nombre de las var a uno más descriptivo */
+  var listaProducto = document.createElement("div")
+  listaProducto.classList.add("producto")
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
+  var titulo = document.createElement("p")
+  titulo.classList.add("titulo")
+  titulo.innerText = productos[i].nombre
   
   var imagen = document.createElement("img");
   imagen.setAttribute('src', productos[i].img);
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
-
-  li.appendChild(d)
+  listaProducto.appendChild(titulo)
+  listaProducto.appendChild(imagen)
+  listaProductos.appendChild(listaProducto)
 }
 
-displayProductos(productos)
+//displayProductos(productos) /* No necesitamos llamar a esa función */
 const botonDeFiltro = document.querySelector("button");
 
-botonDeFiltro.onclick = function() {
-  while (li.firstChild) {
-    li.removeChild(li.firstChild);
+/* Este botón tiene que llamar a la función que realiza el filtrado
+de productos de acuerdo */
+botonDeFiltro.addEventListener = "click", function(){
+  while (listaProductos.firstChild) {
+    listaProductos.removeChild(listaProductos.firstChild);
   }
 
-  const texto = $i.value;
+  /* Aqui leermos el valor ingresado a filtrar y lo mostramos en consola*/
+  const texto = selector.value;
   console.log(texto);
+  /* Obtenemos los productos que coinciden con la búsqueda */
   const productosFiltrados = filtrado(productos, texto );
+  console.log(productoFiltrado);
 
+  /* Me muestra solamente los objetos que pasan el filtro */
   for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
+    var productoFiltrado = document.createElement("div")
+    productoFiltrado.classList.add("producto")
   
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
+    var tituloFiltrado = document.createElement("p")
+    tituloFiltrado.classList.add("titulo")
+    tituloFiltrado.textContent = productosFiltrados[i].nombre
     
     var imagen = document.createElement("img");
     imagen.setAttribute('src', productosFiltrados[i].img);
   
-    d.appendChild(ti)
-    d.appendChild(imagen)
+    productoFiltrado.appendChild(tituloFiltrado)
+    productoFiltrado.appendChild(imagen)
   
-    li.appendChild(d)
+    listaProductos.appendChild(productoFiltrado)
   }
 }
-
+/* Función que determina que elementos coinciden con la búsqueda ya sea en el 
+color o el tipo */
 const filtrado = (productos = [], texto) => {
   return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
 }  
